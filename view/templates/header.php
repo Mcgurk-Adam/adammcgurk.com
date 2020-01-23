c1c4d6ad16e396a447f088750dfef63775570a54
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-01-22 17:49:22 -0700

    Changed things in the CSS

--- view/templates/header.php
+++ view/templates/header.php
@@ -5,4 +5,8 @@
 		<meta charset="utf-8">
 		<title><?=$this->page_title?></title>
 		<link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
+		<link href="/style/style.css" rel="stylesheet">
+
 	</head>
+
+	<header></header>
