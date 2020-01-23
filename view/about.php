56163dccf3d804e98f82f2b4e9ce2e273dcee266
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-01-22 18:22:36 -0700

    Added in the CSS

--- view/about.php
+++ view/about.php
@@ -2,7 +2,9 @@
 
 <main>
 	
-	<img src="/assets/AdamHeadshot.jpg" alt="A picture of Adam smiling into the camera on a bright, sunny day" title="Adam McGurk">
+	<img src="/assets/AdamHeadshot.jpg" alt="A picture of Adam smiling into the camera on a bright, sunny day" title="Adam McGurk" id="adamPicture">
+
+	<h1>About Me</h1>
 
 </main>
 
--- view/templates/header.php
+++ view/templates/header.php
@@ -4,8 +4,9 @@
 	<head>
 		<meta charset="utf-8">
 		<title><?=$this->page_title?></title>
-		<link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Kanit|Unica+One&display=swap" rel="stylesheet">
 		<link href="/style/style.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	</head>
 
