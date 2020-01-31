ce9dc7987971df63e32205692e46a81c0cc0f86e
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-01-31 15:05:23 -0700

    Can't use php strict types

--- model/Page.php
+++ model/Page.php
@@ -1,19 +1,18 @@
 <?php
-declare(strict_types=1);
 
 final class Page {
 
 	public $page_title;
 	public $file_path;
 
-	public function __construct(string $title, string $path) {
+	public function __construct($title, $path) {
 
 		$this->page_title = $title;
 		$this->file_path = $path;
 
 	}
 
-	public function display(array $extra_html = array()): void {
+	public function display($extra_html = array()) {
 		include_once $this->file_path;
 	}
 
