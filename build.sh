45d33f359ad2cfbd16595ce2e2e07393ee3850ce
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-02-17 09:52:54 -0700

    Updated the build file to have all of the routes

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 php index.php > index.html
 php about/index.php > about/index.html
 php contact/index.php > contact/index.html
+php blog/index.php > blog/index.html
+php projects/index.php > projects/index.html
+php experience/index.php > experience/index.html
