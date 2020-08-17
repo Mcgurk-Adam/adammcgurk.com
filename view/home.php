1f26db6b80393207c47aacdd62133fc6589f6ba3
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-08-17 11:55:01 -0500

    Working on this for Upvid

--- view/home.php
+++ view/home.php
@@ -17,92 +17,6 @@
 
 	</section>
 
-<!-- 	<section id="experience" class="white">
-		
-		<div class="content">
-			
-			<h1>Experience<hr></h1>
-
-			<div class="section-flow">
-
-				<div class="content-section">
-					
-					<img src="/assets/shine-solar-logo.svg" alt="Shine Solar" title="Shine Solar">
-
-					<div class="words">
-						
-						<h3>Shine Solar</h3>
-						<span class="position">Lead Developer</span>
-						<span class="date">Feb 2018 - Present</span>
-						<hr>
-
-						<ul>
-							<li>Built the company business intelligence software, <a href="#projects" title="See the projects I've built">Luminary</a>, that (so far) has handled millions of requests a month, over 300,000 calls, 100,000 texts, and maintained 99.9% uptime.</li>
-							<li>Recruited, trained, and managed the software development team (~5 developers total)</li>
-							<li>Worked closely </li>
-						</ul>
-
-					</div>
-
-				</div>
-
-				<hr>
-
-				<div class="content-section switch">
-					
-					<img src="/assets/emperium-logo.png" alt="Shine Solar" title="Shine Solar">
-
-					<div class="words">
-						
-						<h3>Emperium Design</h3>
-						<span class="position">Lead Web Developer</span>
-						<span class="date">Jun 2017 - Jun 2018</span>
-						<hr>
-
-					</div>
-
-				</div>
-
-				<hr>
-
-				<div class="content-section">
-					
-					<img src="/assets/byu-idaho-radio.png" alt="Shine Solar" title="Shine Solar" style="border-radius: 6px;">
-
-					<div class="words">
-						
-						<h3>BYU-Idaho Radio</h3>
-						<span class="position">Chief Engineer</span>
-						<span class="date">Aug 2016 - Apr 2018</span>
-						<hr>
-
-					</div>
-
-				</div>
-
-				<hr>
-
-				<div class="content-section switch">
-					
-					<img src="/assets/krty.jpeg" alt="Shine Solar" title="Shine Solar">
-
-					<div class="words">
-						
-						<h3>KRTY</h3>
-						<span class="position">Engineer</span>
-						<span class="date">Nov 2015 - Aug 2016</span>
-						<hr>
-
-					</div>
-
-				</div>
-
-			</div>
-
-		</div>
-
-	</section> -->
-
 	<section id="projects" class="white">
 		
 		<div class="content">
@@ -173,11 +87,11 @@
 				
 				<div class="tile" data-tech-tags="php html-css ts pwa aws">
 
-					<img src="/assets/shine-solar-mark.svg" alt="Shine Solar Logo" title="Shine Solar">
-					<h3>Signature Generator</h3>
-					<p>Allows employees of Shine Solar to input their data and return a Shine Solar design-approved email signature.</p>
+					<img src="/assets/upvid-mark.svg" alt="Shine Solar Logo" title="Shine Solar">
+					<h3>Upvid</h3>
+					<p>Empowers someone to upload a video and get a shareable URL from that video.</p>
 
-					<a href="" title="Read About the Signature Generator" target="_blank" rel="noopener" class="cta">Read about it</a>
+					<a href="https://upvid.app" title="Visit Upvid" target="_blank" rel="noopener" class="cta">See it</a>
 					
 				</div>
 
