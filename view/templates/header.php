645568545f9623b7bdcf14a565536f573063625a
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-08-17 12:25:23 -0500

    Small header changes

--- view/templates/header.php
+++ view/templates/header.php
@@ -5,7 +5,6 @@
 		<meta charset="utf-8">
 		<title><?=$this->page_title?></title>
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;300;400;500;600&family=Unica+One&display=swap">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;300;400;500;600&family=Unica+One&display=swap" media="print" onload="this.media='all'">
 		<noscript>
