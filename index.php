5b2fdc9aadab697b9a800f7ee98f16509d60d1d8
Adam McGurk <eldermcgurk@gmail.com>
2023-07-13 10:45:22 -0600

    Fixing some of the stuff I used to do

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-if (!isset($_SERVER['REQUEST_URI']) || empty($_SERVER['REQUEST_URI'])) {
+if (empty($_SERVER['REQUEST_URI'])) {
 	$page_path = $argv[1];
 } else {
 	if ($_SERVER['REQUEST_URI'] === '/') {
 		$page_path = 'view/pages/home.php';
 	} else {
 		$parsed_url = parse_url($_SERVER['REQUEST_URI']);
-		$exploded_path = \explode('/', $parsed_url['path']);
-		$page_path = 'view/pages/' . ($exploded_path[\count($exploded_path) - 1] === '' ? $exploded_path[\count($exploded_path) - 2] : $exploded_path[\count($exploded_path) - 1]) . '.php';
+		$exploded_path = explode('/', $parsed_url['path']);
+		$page_path = 'view/pages/' . ($exploded_path[count($exploded_path) - 1] === '' ? $exploded_path[count($exploded_path) - 2] : $exploded_path[count($exploded_path) - 1]) . '.php';
 	}
 }
 
-if (\file_exists($page_path)) {
+if (file_exists($page_path)) {
 	require_once 'view/includes/header.php';
 	require_once $page_path;
 	require_once 'view/includes/footer.php';
