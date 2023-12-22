13515bfd9b194cf51c23cee0c3126a9d4ce76208
Adam McGurk <eldermcgurk@gmail.com>
2023-12-22 16:53:02 -0700

    Changes to the blog

--- view/pages/writing-hub/fiction.php
+++ view/pages/writing-hub/fiction.php
@@ -1,13 +1,15 @@
 <main class="page">
 	<h1>Fictional Works</h1>
     <p>Since I was young, I've always wanted to write fiction. It started with a love for reading, and spun into a love for creation. This is where I publish my short stories.</p>
-    <a class="blog-card" href="/writing-hub/fiction/the-yuletide-encounter" title="The Yuletide Encounter">
+    <hr>
+    <div class="blog-card">
         <div class="image">
-            <img src="/assets/yuletide-encounter.png" title="The image for the Yuletide Encounter" alt="The image for the Yuletide Encounter">
+            <img src="/assets/yuletide-encounter.jpeg" title="The image for the Yuletide Encounter" alt="The image for the Yuletide Encounter">
         </div>
         <div class="words">
-            <h3>The Yuletide Encounter</h3>
+            <h3><a href="/writing-hub/fiction/the-yuletide-encounter" title="The Yuletide Encounter">The Yuletide Encounter</a></h3>
             <p>This is a story I've wanted to write since I was eight years old. I woke up on a Sunday morning sobbing from a dream that I had, and this is the story from that dream.</p>
         </div>
-    </a>
+    </div>
+    <hr>
 </main>
--- view/pages/writing-hub/fiction/the-yuletide-encounter.php
+++ view/pages/writing-hub/fiction/the-yuletide-encounter.php
@@ -1,7 +1,7 @@
 <main class="page blog-post">
 	<h1>The Yuletide Encounter</h1>
 	<figure>
-		<img src="/assets/yuletide-encounter.png" title="The image for the Yuletide Encounter" alt="The image for the Yuletide Encounter">
-		<figcaption>The hope never lost is the hope of innocence</figcaption>
+		<img src="/assets/yuletide-encounter.jpeg" title="The image for the Yuletide Encounter" alt="The image for the Yuletide Encounter">
+		<figcaption>The hope never lost is the hope for innocence</figcaption>
 	</figure>
 </main>
