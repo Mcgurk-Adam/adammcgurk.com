bdd292721205502496ebcb81726b93da5ab3c7e6
Adam McGurk <eldermcgurk@gmail.com>
2023-12-22 16:12:43 -0700

    working

--- view/pages/writing-hub/fiction.php
+++ view/pages/writing-hub/fiction.php
@@ -1,7 +1,7 @@
 <main class="page">
 	<h1>Fictional Works</h1>
     <p>Since I was young, I've always wanted to write fiction. It started with a love for reading, and spun into a love for creation. This is where I publish my short stories.</p>
-    <div class="blog-card">
+    <a class="blog-card" href="/writing-hub/fiction/the-yuletide-encounter" title="The Yuletide Encounter">
         <div class="image">
             <img src="/assets/yuletide-encounter.png" title="The image for the Yuletide Encounter" alt="The image for the Yuletide Encounter">
         </div>
@@ -9,5 +9,5 @@
             <h3>The Yuletide Encounter</h3>
             <p>This is a story I've wanted to write since I was eight years old. I woke up on a Sunday morning sobbing from a dream that I had, and this is the story from that dream.</p>
         </div>
-    </div>
+    </a>
 </main>
---
+++ view/pages/writing-hub/fiction/the-yuletide-encounter.php
@@ -0,0 +1,7 @@
+<main class="page blog-post">
+	<h1>The Yuletide Encounter</h1>
+	<figure>
+		<img src="/assets/yuletide-encounter.png" title="The image for the Yuletide Encounter" alt="The image for the Yuletide Encounter">
+		<figcaption>The hope never lost is the hope of innocence</figcaption>
+	</figure>
+</main>
