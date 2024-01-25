3eb9c5054373307637e9fedf59bc7b3619a8a105
Adam McGurk <eldermcgurk@gmail.com>
2024-01-25 11:23:35 -0700

    Working recipe page here

--- view/pages/recipes.php
+++ view/pages/recipes.php
@@ -18,7 +18,6 @@
             'description' => 'This is my all time favorite dessert. I\'m not a huge dessert guy, but this is my go to every holiday season.',
             'image' => '/assets/apple-blackberry-pie.png',
             'imageAlt' => 'An apple blackberry pie with an oat topping',
-            'imageTitle' => 'An apple blackberry pie with an oat topping',
             'tags' => ['dessert', 'holidays', 'pie'],
         ],
 		[
@@ -26,7 +25,6 @@
 			'description' => 'Such a delicious and simple and fast way of making pot roast',
 			'image' => '/assets/pot-roast.png',
 			'imageAlt' => 'A delicious looking pot roast and vegetables',
-			'imageTitle' => 'A delicious looking pot roast and vegetables',
 			'tags' => ['dinner', 'beef', 'instant-pot'],
 		],
 		[
@@ -34,27 +32,35 @@
 			'description' => 'A delicious creamy florentine sauce with crispy chicken.',
 			'image' => '/assets/chicken-florentine.png',
 			'imageAlt' => 'creamy sauce with chicken and noodles',
-			'imageTitle' => 'creamy sauce with chicken and noodles',
 			'tags' => ['dinner', 'sauce', 'pasta', 'chicken', 'italian'],
+		],
+		[
+			'title' => 'Cajun Pasta',
+			'description' => 'A delicious creamy and spicy cajun pasta',
+			'image' => '/assets/cajun-pasta.png',
+			'imageAlt' => 'penne noodles with parsley, sausage, and a creamy sauce',
+			'tags' => ['dinner', 'sauce', 'pasta', 'sausage', 'italian', 'spicy'],
 		],
     ];
     ?>
-	<?php foreach ($recipes as $recipe): ?>
-        <?php
-            $implodedTitle = implode('-', explode(' ', strtolower($recipe['title'])));
-            $url = '/recipes/' . $implodedTitle;
-        ?>
-        <div class="blog-card" data-tags="<?=implode(' ', $recipe['tags'])?>">
-            <div class="image">
-                <img src="<?=$recipe['image']?>" title="<?=$recipe['imageTitle']?>" alt="<?=$recipe['imageAlt']?>">
+    <div id="recipe-card-container">
+		<?php foreach ($recipes as $recipe): ?>
+			<?php
+			$implodedTitle = implode('-', explode(' ', strtolower($recipe['title'])));
+			$url = '/recipes/' . $implodedTitle;
+			?>
+            <div class="recipe-blog-card" data-tags="<?=implode(' ', $recipe['tags'])?>">
+                <div class="image">
+                    <img src="<?=$recipe['image']?>" title="<?=$recipe['imageTitle'] ?? $recipe['imageAlt']?>" alt="<?=$recipe['imageAlt']?>">
+                </div>
+                <div class="words">
+                    <h3><a href="<?=$url?>" title="<?=$recipe['title']?>"><?=$recipe['title']?></a></h3>
+                    <p><?=$recipe['description']?></p>
+                </div>
+                <hr>
             </div>
-            <div class="words">
-                <h3><a href="<?=$url?>" title="<?=$recipe['title']?>"><?=$recipe['title']?></a></h3>
-                <p><?=$recipe['description']?></p>
-            </div>
-            <hr>
-        </div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+    </div>
 </main>
 
 <script src="/js/recipes.js"></script>
