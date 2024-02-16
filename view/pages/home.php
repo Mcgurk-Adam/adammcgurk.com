86bf5eb9ce18be9ad06f108f5f41b2dabdf75833
Adam McGurk <eldermcgurk@gmail.com>
2024-02-16 14:51:48 -0700

    saving some stuff here

--- view/pages/home.php
+++ view/pages/home.php
@@ -6,6 +6,28 @@
 
     <section class="home-sections">
         <h2>Professional Experience</h2>
+        <div id="mobile-pro-experience"></div>
+        <div id="desktop-pro-experience">
+            <div id="left-pro-experience">
+                <div class="mobile-experience-mark">
+                    <img src="/assets/shine-solar-logo.svg" alt="Shine Solar Logo" title="Shine Solar Logo">
+                    <h3>Shine Solar</h3>
+                </div>
+                <div class="mobile-experience-mark">
+                    <img src="/assets/unbabel-logo.png" alt="Unbabel Logo" title="Unbabel Logo">
+                    <h3>Unbabel</h3>
+                </div>
+                <div class="mobile-clearfix"></div>
+            </div>
+            <div id="middle-bar-pro-experience"></div>
+            <div id="right-pro-experience">
+                <div class="mobile-clearfix"></div>
+                <div class="mobile-experience-mark">
+                    <img src="/assets/onramp-.svg" alt="Shine Solar Logo" title="Shine Solar Logo">
+                    <h3>Shine Solar</h3>
+                </div>
+            </div>
+        </div>
     </section>
 
     <section class="home-sections white">
