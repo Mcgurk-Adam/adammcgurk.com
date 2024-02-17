0093f5b0082328df5c670a58a71578e48c07a9d7
Adam McGurk <amcgurk@bamboohr.com>
2024-02-17 11:40:51 -0700

    working

--- view/pages/home.php
+++ view/pages/home.php
@@ -6,8 +6,7 @@
 
     <section class="home-sections">
         <h2>Professional Experience</h2>
-        <div id="mobile-pro-experience"></div>
-        <div id="desktop-pro-experience">
+        <div id="mobile-pro-experience">
             <div id="left-pro-experience">
                 <div class="mobile-experience-mark">
                     <img src="/assets/shine-solar-logo.svg" alt="Shine Solar Logo" title="Shine Solar Logo">
@@ -23,10 +22,36 @@
             <div id="right-pro-experience">
                 <div class="mobile-clearfix"></div>
                 <div class="mobile-experience-mark">
-                    <img src="/assets/onramp-.svg" alt="Shine Solar Logo" title="Shine Solar Logo">
-                    <h3>Shine Solar</h3>
+                    <img src="/assets/onramp-mark.svg" alt="OnRamp Logo" title="OnRamp Logo">
+                    <h3>OnRamp</h3>
+                </div>
+                <div class="mobile-experience-mark">
+                    <img src="/assets/bamboohr-logo.png" alt="BambooHR Logo" title="BambooHR Logo">
+                    <h3>BambooHR</h3>
                 </div>
             </div>
+
+        </div>
+        <div id="desktop-pro-experience">
+            <div id="logo-container">
+                <div class="desktop-experience-mark">
+                    <img src="/assets/shine-solar-logo.svg" alt="Shine Solar Logo" title="Shine Solar Logo" style="height: 75px;">
+                    <h3>Shine Solar</h3>
+                </div>
+                <div class="desktop-experience-mark">
+                    <img src="/assets/onramp-mark.svg" alt="OnRamp Logo" title="OnRamp Logo" style="height: 55px;">
+                    <h3>OnRamp</h3>
+                </div>
+                <div class="desktop-experience-mark">
+                    <img src="/assets/unbabel-logo.png" alt="Unbabel Logo" title="Unbabel Logo" style="height: 120px; margin-bottom: 0;">
+                    <h3>Unbabel</h3>
+                </div>
+                <div class="desktop-experience-mark">
+                    <img src="/assets/bamboohr-logo.png" alt="BambooHR Logo" title="BambooHR Logo" style="height: 40px;">
+                    <h3>BambooHR</h3>
+                </div>
+            </div>
+            <div id="desktop-timeline"></div>
         </div>
     </section>
 
