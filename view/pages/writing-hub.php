3ff0642c00c63a2a63031b1dc308fbc4a480c291
Adam McGurk <eldermcgurk@gmail.com>
2024-09-19 09:51:12 -0600

    wip

--- view/pages/writing-hub.php
+++ view/pages/writing-hub.php
@@ -1,22 +1,33 @@
+<?php
+    $sections = [
+        [
+			'title' => 'Fiction',
+			'icon_alt' => 'A pencil icon',
+		],
+        [
+			'title' => 'Technology',
+			'icon_alt' => 'An icon showing some code',
+		],
+        [
+			'title' => 'Sports',
+			'icon_alt' => 'A megaphone icon',
+		],
+        [
+			'title' => 'Music',
+			'icon_alt' => 'An icon showing headphones',
+        ],
+    ];
+?>
+
 <main id="writing-hub" class="page">
     <h1>Writing Hub</h1>
     <p>From a young age, reading ignited a passion for writing. Dive into my diverse interests below, from career insights to personal musings. Choose your journey.</p>
     <div class="writing-tile-container">
-        <a class="writing-tile" href="/writing-hub/fiction" title="The fiction section of the writing hub">
-            <h3>Fiction</h3>
-            <img src="/assets/pencil-icon.svg" alt="An icon representing writing fiction" title="An icon representing writing fiction">
-        </a>
-        <a class="writing-tile" href="/writing-hub/technology" title="The fiction section of the writing hub">
-            <h3>Technology</h3>
-            <img src="/assets/code-icon.svg" alt="An icon representing technology" title="An icon representing technology">
-        </a>
-        <a class="writing-tile" href="/writing-hub/sports" title="The sports section of the writing hub">
-            <h3>Sports</h3>
-            <img src="/assets/sports-icon.svg" alt="An icon representing sports" title="An icon representing sports">
-        </a>
-        <a class="writing-tile" href="/writing-hub/essays" title="The essay section of the writing hub">
-            <h3>Blog</h3>
-            <img src="/assets/essay-icon.svg" alt="An icon representing essays" title="An icon representing essays">
-        </a>
+        <?php foreach ($sections as $section): ?>
+            <a class="writing-tile" href="/writing-hub/<?= strtolower($section['title']) ?>" title="The <?= $section['title'] ?> section of the writing hub">
+                <h3><?= $section['title'] ?></h3>
+                <img src="/assets/<?= strtolower($section['title']) ?>-icon.svg" alt="<?= $section['icon_alt'] ?>" title="<?= $section['icon_alt'] ?>">
+            </a>
+        <?php endforeach; ?>
     </div>
 </main>
---
+++ view/pages/writing-hub/music.php
@@ -0,0 +1,4 @@
+<main class="page">
+	<h1>Music</h1>
+	<p>My introduction to the professional world was in country radio. I've always had a passion for country music, both live and recorded. On this page I muse about some of my favorite artists and review songs and albums.</p>
+</main>
