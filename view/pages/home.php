40e81c903bef650ca511f5906aa839d33ea89b60
Adam McGurk <amcgurk@bamboohr.com>
2025-03-27 14:21:08 -0600

    Continuing work on this

--- view/pages/home.php
+++ view/pages/home.php
@@ -7,22 +7,22 @@
     <section class="home-sections">
         <h2>Professional Experience</h2>
         <div id="pro-experience-container">
-            <div class="experience-card">
+            <div class="experience-card animated-away">
                 <img src="/assets/shine-solar-mark.svg" alt="Shine Solar Logo" title="Shine Solar Logo">
                 <h3>Shine Solar</h3>
                 <button type="button" class="light-blue small" data-opens="#experience-modal" data-link="https://shinesolar.com" data-title="Shine Solar" data-img="/assets/shine-solar-mark.svg" data-undertext="Lead Developer" data-points="Was the first engineering hire and grew the team to seven.|Built the company's mobile app <a href='https://shinesolar.app' title='Shine Solar App' target='_blank' rel='noopener'>The Shine Solar App</a>, supporting over 1000 customers during and after their installation.|Built and maintained the company's custom CRM and ERP <a href='https://luminaryapps.io' title='Luminary' target='_blank' rel='noopener'>Luminary</a> which supported thousands of calls a day and tens of millions of dollars a month.|Built and maintained over 50 websites for the company.">Learn More</button>
             </div>
-            <div class="experience-card">
+            <div class="experience-card animated-away">
                 <img src="/assets/unbabel-mark.svg" alt="Unbabel Logo" title="Unbabel Logo">
                 <h3>Unbabel</h3>
                 <button type="button" class="light-blue small" data-opens="#experience-modal" data-link="https://unbabel.com" data-title="Unbabel" data-img="/assets/unbabel-mark.svg" data-undertext="Software Engineering Consultant" data-points="Built and maintained the company's <a href='https://appsource.microsoft.com/en-us/product/dynamics-365/unbabel1586431770145.unbabel' title='Microsoft Dynamics app link' target='_blank' rel='noopener'>Microsoft Dynamics App</a>, powering translation for thousands of customer emails a month.|Pioneered fully automated CI/CD for testing, building, and delivering a Microsoft App Source app on Github Actions.|Worked closely with Fortune 500 companies to support their specific needs and iterate on the integration build.|Built a micro blog around my learnings and findings building a Microsoft Dynamics app - <a href='https://dynamicsdevhub.com' title='DynamicsDevHub link'>DynamicsDevHub</a>.">Learn More</button>
             </div>
-            <div class="experience-card">
+            <div class="experience-card animated-away">
                 <img src="/assets/onramp-mark.svg" alt="OnRamp Logo" title="OnRamp Logo">
                 <h3>OnRamp</h3>
                 <button type="button" class="light-blue small" data-opens="#experience-modal" data-link="https://onramp.us" data-title="OnRamp" data-img="/assets/onramp-mark.svg" data-undertext="Software Engineering Consultant" data-points="Built and maintained the company's full integration suite, including full integrations with Salesforce, Hubspot, Slack, and more.|Built and maintained the company's main application SSO implementation, inclusive of both Google SSO and SAML SSO.|Built the company's queueing system that processed emails, webhook reception, and integration processing.|Worked closely with customers on specific customer issues as they arose.">Learn More</button>
             </div>
-            <div class="experience-card">
+            <div class="experience-card animated-away">
                 <img src="/assets/bamboohr-mark.svg" alt="BambooHR Logo Mark" title="BambooHR Logo Mark">
                 <h3>BambooHR</h3>
                 <button type="button" class="light-blue small" data-opens="#experience-modal" data-link="https://bamboohr.com" data-title="BambooHR" data-img="/assets/bamboohr-mark.svg" data-undertext="Senior Software Engineer, Backend. Tech Lead" data-points="Was the founding tech lead of the Customer Lifecycle team, working on customer signup, in app implementation, and cancellation.|Shipped a revamped cancellation experience that decreased total customer support tickets by 15% in the company.|Pioneered AI in the product by introducing the first in product feature that used AI, a data cleaning feature for onboarding.|Led the team as we built a completely reimagined onboarding experience, allowing our customer success team to adopt 25% more customers with no increase in internal headcount.|Transitioned to the tech lead of the AI Experience team, building AI powered chat called BambooHR<sup>&reg;</sup> Answers">Learn More</button>
@@ -33,17 +33,17 @@
     <section class="home-sections white">
         <h2>Projects</h2>
         <div id="project-container">
-            <div class="project-card">
+            <div class="project-card animated-away">
                 <img src="/assets/cloud-kill-switch-logo.svg" alt="Cloud Kill Switch logo" title="Cloud Kill Switch logo">
                 <h3>Cloud Kill Switch</h3>
                 <a href="https://cloudkillswitch.com/" title="Cloud Kill Switch Link" class="button light-blue" target="_blank" rel="noopener">Visit Website</a>
             </div>
-            <div class="project-card">
+            <div class="project-card animated-away">
                 <img src="/assets/optimize-mark.svg" alt="OptimizeFonts logo" title="OptimizeFonts logo">
                 <h3>Optimize Fonts</h3>
                 <a href="https://optimizefonts.com/" title="OptimizeFonts Link" class="button yellow" target="_blank" rel="noopener">Visit Website</a>
             </div>
-            <div class="project-card">
+            <div class="project-card animated-away">
                 <img src="/assets/quitplanetfitnesslogo.svg" alt="Quit Planet Fitness logo" title="Quit Planet Fitness logo">
                 <h3>Quit Planet Fitness</h3>
                 <a href="https://quitplanetfitness.com/" title="Quit Planet Fitness Link" class="button red" target="_blank" rel="noopener">Visit Website</a>
