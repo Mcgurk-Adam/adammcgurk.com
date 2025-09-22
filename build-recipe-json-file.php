c86fca22c1018354fae12b730768d45c4633e2b6
Adam McGurk <amcgurk@bamboohr.com>
2025-09-22 12:54:01 -0600

    further refining

--- build-recipe-json-file.php
+++ build-recipe-json-file.php
@@ -4,7 +4,34 @@ require_once "vendor/autoload.php";
 require_once "env.php";
 require_once "php_utils/cloudflare.php";
 
-$megaJson = [];
+$recipeDbResponse = queryDb('SELECT id, title, image_name as image, image_alt as imageAlt FROM recipes');
+$recipeJson = $recipeDbResponse['result'][0]['results'];
 
-$recipeDbResponse = queryDb('SELECT title, image_name as image, image_alt FROM recipes');
-var_dump($recipeDbResponse);
+foreach ($recipeJson as &$recipe) {
+	$recipeId = $recipe['id'];
+	unset($recipe['id']);
+	$recipe['tags'] = array_map(function ($tag) {
+		return $tag['tag_name'];
+	}, queryDb('SELECT tag_name FROM recipe_tags WHERE recipe_id = ?', [$recipeId])['result'][0]['results']);
+	$recipe['steps'] = array_map(function ($step) {
+		return $step['step_value'];
+	}, queryDb('SELECT step_value FROM recipe_steps WHERE recipe_id = ?', [$recipeId])['result'][0]['results']);
+	$recipe['ingredients'] = array_map(function ($ingredient) {
+		return $ingredient['ingredient_value'];
+	}, queryDb('SELECT ingredient_value FROM recipe_ingredients WHERE recipe_id = ?', [$recipeId])['result'][0]['results']);
+	$recipeSections = queryDb('SELECT id, section_name FROM recipe_ingredient_sections WHERE recipe_id = ?', [$recipeId])['result'][0]['results'];
+	foreach ($recipeSections as &$recipeSection) {
+		$sectionJson = [
+			'sectionTitle' => $recipeSection['section_name'],
+		];
+		$recipeSectionId = $recipeSection['id'];
+		unset($recipeSection['id']);
+		$recipeSection['sectionIngredients'] = array_map(function ($ingredient) {
+			return $ingredient['ingredient_value'];
+		}, queryDb('SELECT ingredient_value FROM recipe_ingredients WHERE section_id = ?', [$recipeSectionId])['result'][0]['results']);
+		$recipe['ingredients'][] = $recipeSection;
+	}
+}
+
+unlink('test-recipes.json');
+file_put_contents('test-recipes.json', json_encode($recipeJson));
