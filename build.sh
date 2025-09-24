e45f8c46c618a742db9cf2492ac7c76721c255ce
Adam McGurk <eldermcgurk@gmail.com>
2025-09-24 13:25:27 -0600

    Added in the recipes build step

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ node_modules/.bin/tsc -p ts/recipes/
 node_modules/.bin/tsc -p ts/recipes/chat/
 composer install
 
-php build-recipe-json-file.php
+npx wrangler r2 object get "adammcgurk-com/recipes.json" --file "./recipes.json"
 
 # build php pages
 for f in $(find view/pages -name '*.php')
